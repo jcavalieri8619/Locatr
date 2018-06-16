8d311fee6a338b9fcaafc706904472324d45e54d
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-16 00:05:15 -0400

    added googleAPI availability check

--- app/src/main/java/com/bignerdranch/android/locatr/ui/LocatrActivity.java
+++ app/src/main/java/com/bignerdranch/android/locatr/ui/LocatrActivity.java
@@ -1,12 +1,41 @@
 package com.bignerdranch.android.locatr.ui;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class LocatrActivity extends SingleFragmentActivity {
+
+    private static final int REQUEST_ERROR = 0;
 
     @Override
     protected Fragment createFragment() {
         return LocatrFragment.newInstance("", "");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = apiAvailability.getErrorDialog(this,
+                    errorCode, REQUEST_ERROR, new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(final DialogInterface dialog) {
+                            finish();
+
+                        }
+                    });
+            errorDialog.show();
+
+        }
+    }
 }
