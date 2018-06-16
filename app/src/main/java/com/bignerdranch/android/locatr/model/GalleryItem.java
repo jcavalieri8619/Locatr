335ac550eee00e76dc04bb8b30bd55861e58c60e
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-15 23:44:06 -0400

    model of Flickr Photo

---
+++ app/src/main/java/com/bignerdranch/android/locatr/model/GalleryItem.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.locatr.model;
+
+public interface GalleryItem {
+
+    String getHeight();
+
+    void setHeight(String height);
+
+    void setWidth(String width);
+
+    String getWidth();
+
+    String getID();
+
+    void setID(String ID);
+
+    String getOwner();
+
+    void setOwner(String owner);
+
+    String getCaption();
+
+    void setCaption(String caption);
+
+
+    String getUrl();
+
+    void setUrl(String url);
+
+
+}
