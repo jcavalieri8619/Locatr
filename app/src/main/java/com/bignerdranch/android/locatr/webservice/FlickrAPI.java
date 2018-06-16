d2b116de531666aeaeb067fd8d8ded6fc20adcc5
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-15 23:47:24 -0400

    retrofit based Flickr API webservice

---
+++ app/src/main/java/com/bignerdranch/android/locatr/webservice/FlickrAPI.java
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.locatr.webservice;
+
+import com.bignerdranch.android.locatr.GalleryItemWrapper;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface FlickrAPI {
+
+    String BASE_URL = "https://api.flickr.com/";
+    String API_KEY = "156d82fd9e142d5482f873c8d85f71ce";
+
+
+    @GET("services/rest/?method=flickr.photos.getRecent")
+    Single<GalleryItemWrapper> loadRecents();
+
+
+    @GET("services/rest/?method=flickr.photos.search")
+    Single<GalleryItemWrapper> loadSearchResults(@Query("text") String text);
+
+
+}
