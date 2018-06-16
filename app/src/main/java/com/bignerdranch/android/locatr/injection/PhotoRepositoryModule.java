40703e0ba1b16ff489179a490bb52ed0e2acb6ec
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-15 23:43:34 -0400

    dagger module for providing dependencies PhotoRepo

---
+++ app/src/main/java/com/bignerdranch/android/locatr/injection/PhotoRepositoryModule.java
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.locatr.injection;
+
+import com.bignerdranch.android.locatr.repository.PhotoRepository;
+import com.bignerdranch.android.locatr.repository.PhotoRepositoryImpl;
+import com.bignerdranch.android.locatr.webservice.FlickrAPI;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module(includes = FlickrWebServiceModule.class)
+public class PhotoRepositoryModule {
+
+    @Singleton
+    @Provides
+    public PhotoRepository photoRepository(FlickrAPI webService) {
+        return new PhotoRepositoryImpl(webService);
+    }
+}
