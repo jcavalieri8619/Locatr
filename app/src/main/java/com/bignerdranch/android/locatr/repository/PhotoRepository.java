bd038c078787871d8c8d047bcbaf3de52dd033ac
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-15 23:44:22 -0400

    photo repo interface

---
+++ app/src/main/java/com/bignerdranch/android/locatr/repository/PhotoRepository.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.locatr.repository;
+
+import com.bignerdranch.android.locatr.GalleryItemEntity;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+public interface PhotoRepository {
+
+    Single<List<GalleryItemEntity>> searchPhotos(String text);
+
+    Single<List<GalleryItemEntity>> getRecentPhotos();
+
+
+}
