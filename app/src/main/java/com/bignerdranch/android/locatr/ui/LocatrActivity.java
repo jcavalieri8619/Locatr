85d7cdeca29e825fe8e57fb81faed5c51d20b926
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-15 23:45:55 -0400

    refactored

--- app/src/main/java/com/bignerdranch/android/locatr/ui/LocatrActivity.java
+++ app/src/main/java/com/bignerdranch/android/locatr/ui/LocatrActivity.java
@@ -1,8 +1,6 @@
-package com.bignerdranch.android.locatr;
+package com.bignerdranch.android.locatr.ui;
 
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class LocatrActivity extends SingleFragmentActivity {
 
