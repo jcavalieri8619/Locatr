cc7362941cc8ea70a3d5b23a305452b7dbc541fa
John P Cavalieri <jcavalieri8619@gmail.com>
2018-10-17 16:32:31 -0400

    added method fetching by location

--- app/src/main/java/com/bignerdranch/android/locatr/repository/PhotoRepository.java
+++ app/src/main/java/com/bignerdranch/android/locatr/repository/PhotoRepository.java
@@ -1,16 +1,19 @@
 package com.bignerdranch.android.locatr.repository;
 
 import com.bignerdranch.android.locatr.model.GalleryItemEntity;
+import com.bignerdranch.android.locatr.model.GalleryItemWrapper;
+import com.bignerdranch.android.locatr.webservice.FlickrAPI;
 
 import java.util.List;
 
 import io.reactivex.Single;
 
-public interface PhotoRepository {
+public interface PhotoRepository{
 
-    Single<List<GalleryItemEntity>> searchPhotos(String text);
+    Single<List<GalleryItemEntity>> searchPhotosByText(String text);
 
     Single<List<GalleryItemEntity>> getRecentPhotos();
 
 
+    Single<List<GalleryItemEntity>> searchPhotosByLocation(String latitute, String longitute);
 }
