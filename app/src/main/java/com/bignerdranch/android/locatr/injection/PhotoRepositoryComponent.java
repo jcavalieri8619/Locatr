a8beaa8c2cf116ee31139598f11d7b4eb121c1db
John P Cavalieri <jcavalieri8619@gmail.com>
2018-10-17 16:30:10 -0400

    changed injection style

--- app/src/main/java/com/bignerdranch/android/locatr/injection/PhotoRepositoryComponent.java
+++ app/src/main/java/com/bignerdranch/android/locatr/injection/PhotoRepositoryComponent.java
@@ -1,16 +1,15 @@
 package com.bignerdranch.android.locatr.injection;
 
 
+import com.bignerdranch.android.locatr.repository.PhotoRepository;
 import com.bignerdranch.android.locatr.ui.LocatrViewModel;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
 
-@Singleton
 @Component(modules = PhotoRepositoryModule.class)
 public interface PhotoRepositoryComponent {
 
-    void inject(LocatrViewModel viewModel);
-
+    PhotoRepository injectPhotoRepository();
 }
